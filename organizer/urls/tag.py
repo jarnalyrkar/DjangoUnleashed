a073e567f3e8c67debb368967b667e307e4c312c
jarnalyrkar <joachimsolstrom@gmail.com>
2017-07-08 20:05:16 +0200

    First commit up to chapter 13, i believe..

---
+++ organizer/urls/tag.py
@@ -0,0 +1,26 @@
+from django.conf.urls import url
+
+from ..views import (
+    TagPageList, TagList, TagCreate, TagDelete, TagUpdate, tag_detail,
+    tag_list)
+
+urlpatterns = [
+    url(r'^$',
+        TagList.as_view(),
+        name='organizer_tag_list'),
+    url(r'^create/$',
+        TagCreate.as_view(),
+        name='organizer_tag_create'),
+    url(r'^(?P<page_number>\d+)/$',
+        TagPageList.as_view(),
+        name='organizer_tag_page'),
+    url(r'^(?P<slug>[\w\-]+)/$',
+        tag_detail,
+        name='organizer_tag_detail'),
+    url(r'^(?P<slug>[\w-]+)/delete/$',
+        TagDelete.as_view(),
+        name='organizer_tag_delete'),
+    url(r'^(?P<slug>[\w\-]+)/update/$',
+        TagUpdate.as_view(),
+        name='organizer_tag_update'),
+]
